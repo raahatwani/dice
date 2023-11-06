0fe706b11b0b21c56dfc28885f9d42c1e7f4dbcc
Raahat Wani <raahatwani@gmail.com>
2023-11-06 20:56:16 +0530

    modified:   android/build.gradle 	modified:   lib/main.dart 	modified:   pubspec.lock 	deleted:    test/widget_test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -28,55 +30,59 @@ class _MyAppState extends State<MyApp> {
           centerTitle: true,
           backgroundColor: Colors.red.shade500,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  child: GestureDetector(
-                    onTap: () {
-                      leftdice = Random().nextInt(6) + 1;
-                      rightdice = Random().nextInt(6) + 1;
-                      leftdice == rightdice
-                          ? diceButton = false
-                          : diceButton = true;
-                      setState(() {});
-                    },
-                    child: Image.asset('images/$leftdice.png'),
+        body: Container(
+          margin: EdgeInsets.only(left: 8,right: 8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        leftdice = Random().nextInt(6) + 1;
+                        rightdice = Random().nextInt(6) + 1;
+                        leftdice == rightdice
+                            ? diceButton = false
+                            : diceButton = true;
+                        setState(() {});
+                      },
+                      child: Image.asset('images/$leftdice.png'),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  child: GestureDetector(
-                    onTap: () {
-                      leftdice = Random().nextInt(6) + 1;
-                      rightdice = Random().nextInt(6) + 1;
-                      leftdice == rightdice
-                          ? diceButton = false
-                          : diceButton = true;
-                      setState(() {});
-                    },
-                    child: Image.asset('images/$rightdice.png'),
+                  SizedBox(width: 5,),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        leftdice = Random().nextInt(6) + 1;
+                        rightdice = Random().nextInt(6) + 1;
+                        leftdice == rightdice
+                            ? diceButton = false
+                            : diceButton = true;
+                        setState(() {});
+                      },
+                      child: Image.asset('images/$rightdice.png'),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            diceButton
-                ? SizedBox()
-                : ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: StadiumBorder(),
-                        backgroundColor: Colors.red.shade400),
-                    onPressed: () {},
-                    child: Text(
-                      'you win',
-                      style: TextStyle(color: Colors.white, fontSize: 60),
-                    ))
-          ],
+                ],
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              diceButton
+                  ? SizedBox()
+                  : ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          backgroundColor: Colors.green.shade400),
+                      onPressed: () {},
+                      child: Text(
+                        'you win',
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ))
+            ],
+          ),
         ),
       ),
     );
